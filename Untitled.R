49098fd810b7fa303e696b469b745119637f0023
JiayiJoyce <jiayijoyce@163.com>
2023-10-25 23:02:49 +0100

    Update Untitled.R

--- Untitled.R
+++ Untitled.R
@@ -1,14 +1,3 @@
-# Define working directory 
-working_directory <- "working/dic/"
 
-# get list of current file 
-# names as character vector current_files 
-current_files <- list.files(working_directory) 
 
-# get file names after renaming 
-# as character vector new_files 
-new_files <- c("Workshop-Practice.R") 
 
-# Use file.rename() function to rename files 
-file.rename(paste0(working_directory, current_files), 
-            paste0(working_directory, new_files))
